f8ee2675e04f19a9ed33c68e528ed96cdc05066b
Dexter <ddocallas1108@gmail.com>
2025-09-28 00:26:57 +0800

    Update apps/backend/src/modules/products/model/products.js
    Adjust min prices to 0
    
    Co-authored-by: Jay Are Galinada <jayaregalinada@gmail.com>

--- apps/backend/src/modules/products/model/products.js
+++ apps/backend/src/modules/products/model/products.js
@@ -21,13 +21,11 @@ const productSchema = new Schema({
   },
   purchasePrice: {
     type: Number,
-    min: [1, 'Purchase price must be at least 1'],
-    required: true,
+    min: [0, 'Purchase price must be at least 0'],
   },
   sellingPrice: {
     type: Number,
-    min: [1, 'Selling price must be at least 1'],
-    required: true,
+    min: [0, 'Selling price must be at least 0'],
   },
   qty: {
     type: Number,
