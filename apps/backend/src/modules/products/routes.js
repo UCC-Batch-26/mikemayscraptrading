f41c602f66fccacf4707407488cdc091adf25bfe
Dexter Docallas <dexterdocallas@Dexters-MacBook-Pro.local>
2025-09-28 01:56:40 +0800

    feat: connected getProduct to routes

--- apps/backend/src/modules/products/routes.js
+++ apps/backend/src/modules/products/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { getAllProducts } from './controllers/get-all-products.js';
+import { getProduct } from './controllers/get-product.js';
 
 const router = new Router();
 
 router.get('/', getAllProducts);
+router.get('/:id', getProduct);
 
 export default router;
