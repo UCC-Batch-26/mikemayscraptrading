784c8050dbff7822da9111df15a40f8a87fede02
Dexter Docallas <dexterdocallas@Dexters-MacBook-Pro.local>
2025-10-02 03:26:42 +0800

    feat: connected get-inventory-transaction to routes

--- apps/backend/src/modules/inventory-transactions/routes.js
+++ apps/backend/src/modules/inventory-transactions/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { addInventoryTransaction } from './controllers/add-inventory-transaction';
 import { getAllInventoryTransactions } from './controllers/get-all-inventory-transactions';
+import { getInventoryTransaction } from './controllers/get-inventory-transaction';
 
 const router = new Router();
 
 router.post('/', addInventoryTransaction);
 router.get('/', getAllInventoryTransactions);
+router.get('/:id', getInventoryTransaction);
 
 export default router;
