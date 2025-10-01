8f99007140cc441c5b9332dea75554ede645ee98
Dexter Docallas <dexterdocallas@Dexters-MacBook-Pro.local>
2025-10-02 02:36:44 +0800

    feat: added add inventory transaction

---
+++ apps/backend/src/modules/inventory-transactions/controllers/add-inventory-transaction.js
@@ -0,0 +1,20 @@
+import { InventoryTransaction } from "#models/inventory-transactions";
+import { log } from "#utils/log.js";
+
+export async function addInventoryTransaction(req, res) {
+  try {
+    const { transactionType, quantityChange } = req.body;
+    const inventoryTransaction = await InventoryTransaction.create({ transactionType, quantityChange });
+
+    return res.status(201).json({
+      message: 'Successfully created inventory transaction',
+      data: inventoryTransaction,
+    });
+  } catch (error) {
+    log('addInventoryTransaction', 'Error creating inventory transaction:', error);
+
+    return res.status(400).json({
+      message: error?.message ?? 'Something went wrong creating sample',
+    });
+  }
+}
