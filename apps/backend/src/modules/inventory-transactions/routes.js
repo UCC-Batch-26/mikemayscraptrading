b1e61a9a9a181e35ca10d53f35cd8f09b9d49869
Dexter Docallas <dexterdocallas@Dexters-MacBook-Pro.local>
2025-10-02 03:19:05 +0800

    feat: added get-all-inventory-transactions to controller

--- apps/backend/src/modules/inventory-transactions/routes.js
+++ apps/backend/src/modules/inventory-transactions/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { addInventoryTransaction } from './controllers/add-inventory-transaction';
+import { getAllInventoryTransactions } from './controllers/get-all-inventory-transactions';
 
 const router = new Router();
 
 router.post('/', addInventoryTransaction);
+router.get('/', getAllInventoryTransactions);
 
 export default router;
