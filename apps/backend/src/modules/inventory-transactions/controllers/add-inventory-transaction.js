afb97d954a9bda0ca4e55052c033f235d07d276f
dexterdocallas <ddocallas1108@gmail.com>
2025-10-03 00:51:16 +0800

    fix: inventory-transaction model import alias

--- apps/backend/src/modules/inventory-transactions/controllers/add-inventory-transaction.js
+++ apps/backend/src/modules/inventory-transactions/controllers/add-inventory-transaction.js
@@ -1,4 +1,4 @@
-import { InventoryTransaction } from '#models/inventory-transactions';
+import { InventoryTransaction } from '#modules/inventory-transactions/models/inventory-transactions';
 import { log } from '#utils/log.js';
 
 export async function addInventoryTransaction(req, res) {
